b509ccdf2f958f3f903429ca609825d2c3449552
USUARIO <oscarcortina12@gmail.com>
2020-03-31 20:10:18 -0500

    agregue mapa y bottom navigation wiew

--- app/src/main/java/com/example/myapplication/IndexCliente.java
+++ app/src/main/java/com/example/myapplication/IndexCliente.java
@@ -1,15 +1,136 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class IndexCliente extends AppCompatActivity {
+import com.example.myapplication.Servicios.ListarEventosService;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class IndexCliente extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_index_cliente);
+        RevisarPermisos();
+        setContentView(R.layout.activity_index_cliente2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        View itemEvento = (View) findViewById(R.id.item_eventos);
 
+        itemEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListarEventosService servicio = new ListarEventosService(IndexCliente.this);
+                servicio.execute();
+            }
+        });
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(10.4645477, -73.275759);
+        LatLng valledupar = new LatLng(getMyPosition().getLatitude(), getMyPosition().getLongitude());
+        mMap.addMarker(new MarkerOptions().position(valledupar).title("Valledupar"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(valledupar,16));
+    }
+
+    public Location getMyPosition(){
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        return  location;
+    }
+
+    public void RevisarPermisos(){
+        int permissionCheck = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            }else {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+        }
+        permissionCheck = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        //Add a marker in Sydney and move the camera
+        Location my_posicion = getMyPosition();
+        double lat;
+        double lon;
+        if (my_posicion==null){
+            //colocamos la localizacion de colombia si no lee la actual
+            lat = 2.8894434;
+            lon = -73.783892;
+            //Snackbar.make(getCurrentFocus(), "Por favor asegurese de tener el GPS encendido es necesario para el uso de la aplicacion", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            Toast.makeText(this, "Por favor asegurese de tener el GPS encendido es necesario para el uso de la aplicacion", Toast.LENGTH_LONG).show();
+        }else{
+            lat = my_posicion.getLatitude();
+            lon = my_posicion.getLongitude();
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Routes/Routes.java
+++ app/src/main/java/com/example/myapplication/Routes/Routes.java
@@ -1,7 +1,7 @@
 package com.example.myapplication.Routes;
 
 public class Routes {
-    public static String server = "http://192.168.1.21:8000/";
+    public static String server = "http://192.168.1.15:8000/";
     public static String validar_login = server+"validarLogin";
     public static String crear_cuenta = server+"crearCuenta";
     public static String listar_eventos = server+"listarEventos";
